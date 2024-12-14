766377fecb3b82890c85decf043780f8777748cd
secondW4ve <yshchur@mycubes.nl>
2024-12-14 20:24:27 +0200

    Backword clock for bomb

--- 7segmentLed/7_segment_led/7_segment_led.ino
+++ 7segmentLed/7_segment_led/7_segment_led.ino
@@ -8,23 +8,25 @@ int counter;
 
 int centerX = -1;
 int centerY = -1;
-int maxX = 0;
-int minX = 1024;
-int maxY = 0;
-int minY = 1024;
+int maxX = 1023;
+int minX = 0;
+int maxY = 1023;
+int minY = 0;
 
 bool settingTimer = true;
 bool displayState = true;
 
 unsigned long lastUpdateTime = 0;
+unsigned long lastBlinkTime = 0;
 unsigned long lastInputCheckTime = 0;
 
 const unsigned long blinkInterval = 500;
 
-int minutes1 = 0;
-int minutes2 = 0;
-int seconds1 = 0;
-int seconds2 = 0;
+int timer[] = { 0, 0, 0, 0 };
+// int minutes1 = 0;
+// int minutes2 = 0;
+// int seconds1 = 0;
+// int seconds2 = 0;
 int selectedNumber = 0;
 
 const unsigned long dotBlinkInterval = 500;
@@ -32,11 +34,12 @@ const unsigned long debounceInterval = 300;
 
 unsigned long lastClockUpdate = 0;
 unsigned long lastDotBlinkUpdate = 0;
-
 bool dotClockState = true;
 
-bool editingMode = false;
+bool boom = false;
 
+byte digitOff = 0x00;
+byte digitOffDotOn = 0x80;
 byte digits[] = {
   0x3F, // '0'
   0x06, // '1'
@@ -47,7 +50,7 @@ byte digits[] = {
   0x7D, // '6'
   0x07, // '7'
   0x7F, // '8'
-  0x6F // '9'
+  0x6F  // '9'
 };
 
 byte digitsWithDot[] = {
@@ -62,8 +65,6 @@ byte digitsWithDot[] = {
   0x7F | 0x80, // '8' with dot
   0x6F | 0x80  // '9' with dot
 };
-
-unsigned long currentIndex = 0;
 
 void setup(){
   byte numDigits = 4;
@@ -86,47 +87,102 @@ void setup(){
   Serial.print(F(", center Y: "));
   Serial.print(centerY);
   Serial.println();
+  for (int i = 0; i < 4; i++) {
+    sevseg.setSegmentsDigit(timer[i], digits[0]);
+  }
+  sevseg.refreshDisplay();
 }
 
-// 0x00 - off
-// 0x01 - segment A
-// 0x02 - segment B
-// 0x03 - segment A + segment B
-// 0x04 - segment C
-// 0x05 - segment A + segment C
-// 0x06 - segment B + segment C
-// 0x07 - segment A + segment B + segment C
-// 0x08 - segment D
-
 void loop() {
-  unsigned long now = millis();
-  sevseg.setSegmentsDigit(0, digits[currentIndex]);
-  sevseg.setSegmentsDigit(2, digitsWithDot[currentIndex]);
-  sevseg.refreshDisplay();
-  if (now - lastUpdateTime >= 1000) {
-    lastUpdateTime = now;
-    currentIndex = currentIndex == 9 ? 0 : currentIndex + 1;
+  if (boom) {
+    sevseg.setSegmentsDigit(0, digits[8]);
+    sevseg.setSegmentsDigit(1, digits[8]);
+    sevseg.setSegmentsDigit(2, digits[8]);
+    sevseg.setSegmentsDigit(3, digits[8]);
+    sevseg.refreshDisplay();
+    return;
   }
- // Array of hex values for digits 0-9 with the dot included
-  // if (settingTimer) {
-  //    if (now - lastBlinkTime >= blinkInterval) {
-  //     lastBlinkTime = now;
-  //     displayState = !displayState;
-  //     if (displayState) {
-  //       int timer = 1000 * minutes1 + 100 * minutes2 + 10 * seconds1 + seconds2;
-  //       sevseg.setNumber(timer, 2);
-  //       sevseg.setSegmentsDigit(0, 1);
-  //     } else {
-  //       sevseg.blank();
-  //     }
-  //    }
+  unsigned long now = millis();
+  int btnState = digitalRead(SW_PIN);
+  if (btnState == LOW) {
+    settingTimer = false;
+  }
 
-  //    if (now - lastInputCheckTime >= debounceInterval) {
-  //     lastInputCheckTime = now;
-  //     int posX = analogRead(VRXPin);
-  //     int posY = analogRead(VRYPin);
-  //    }
-  // }
+  if (settingTimer) {
+    if (now - lastBlinkTime >= blinkInterval) {
+      lastBlinkTime = now;
+      displayState = !displayState;
+      if (displayState) {
+        sevseg.setSegmentsDigit(selectedNumber, selectedNumber == 1 ? digitsWithDot[timer[selectedNumber]] : digits[timer[selectedNumber]]);
+      } else {
+        sevseg.setSegmentsDigit(selectedNumber, selectedNumber == 1 ? digitOffDotOn : digitOff);
+      }
+    }
+
+    if (now - lastInputCheckTime >= debounceInterval) {
+      lastInputCheckTime = now;
+      int posX = analogRead(VRXPin);
+      int posY = analogRead(VRYPin);
+
+      if (posX > centerX + 400) {
+        timer[selectedNumber] = timer[selectedNumber] == 0 ? 0 : timer[selectedNumber] - 1;
+      } else if (posX < centerX - 400) {
+        timer[selectedNumber] = timer[selectedNumber] == 9 ? 0 : timer[selectedNumber] + 1;
+      } else if (posY > centerY + 400) {
+        selectedNumber = selectedNumber == 0 ? 3 : selectedNumber - 1;
+      } else if (posY < centerY - 400) {
+        selectedNumber = selectedNumber == 3 ? 0 : selectedNumber + 1;
+      }
+    }
+
+    for (int i = 0; i < 4; i++) {
+      if (i != selectedNumber) {
+        sevseg.setSegmentsDigit(i, i == 1 ? digitsWithDot[timer[i]] : digits[timer[i]]);
+      }
+    }
+  } else {
+    if (now - lastClockUpdate > 1000) {
+      lastClockUpdate = now;
+      if (timer[0] == 0 && timer[1] == 0 && timer[2] == 0 && timer[3] == 0) {
+        Serial.println(F("BOOOOOM!!!!"));
+        sevseg.setSegmentsDigit(0, digits[8]);
+        sevseg.setSegmentsDigit(1, digits[8]);
+        sevseg.setSegmentsDigit(2, digits[8]);
+        sevseg.setSegmentsDigit(3, digits[8]);
+        boom = true;
+        return;
+      }
+      timer[3] = timer[3] == 0 ? 9 : timer[3] - 1;
+      if (timer[3] == 9) {
+        timer[2] = timer[2] == 0 ? 5 : timer[2] - 1;
+
+        if (timer[2] == 5) {
+          timer[1] = timer[1] == 0 ? 9 : timer[1] - 1;
+
+          if (timer[1] == 9 && timer[0] != 0) {
+            timer[0] = timer[0] - 1;
+          }
+        }
+      }
+
+      for (int i = 0; i < 4; i++) {
+        sevseg.setSegmentsDigit(i, digits[timer[i]]);
+      }
+    }
+
+    if (now - lastDotBlinkUpdate > dotBlinkInterval) {
+      lastDotBlinkUpdate = now;
+      dotClockState = !dotClockState;
+
+      if (!dotClockState) {
+        sevseg.setSegmentsDigit(1, digits[timer[1]]);
+      } else {
+        sevseg.setSegmentsDigit(1, digitsWithDot[timer[1]]);
+      }
+    }
+  }
+
+  sevseg.refreshDisplay();
 //  static bool lastButtonState = HIGH;
 //  int btnState = digitalRead(SW_PIN);
 //  if (btnState == LOW && lastButtonState == HIGH) {
